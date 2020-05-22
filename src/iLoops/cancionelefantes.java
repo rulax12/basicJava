03df51341c1aa13cda4c680598d337ad4110f3d3
kmpw648 <rulas12@gmail.com>
2020-05-22 14:28:08 -0500

    First Commit

---
+++ src/iLoops/cancionelefantes.java
@@ -0,0 +1,9 @@
+package iLoops;
+
+public class cancionelefantes {
+    public static void main (String[] args) {
+        for (int i = 1; i <= 100; i++) {
+            System.out.println(i + " se columpiaban sobre la tela de una araña como veia que resistia fueron a llamar a otro elefante");
+        }
+    }
+}
